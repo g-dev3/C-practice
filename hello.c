b1c62581dc3f2d11afc0ca76c299295e8603384d
G Developer <157769824+g-dev3@users.noreply.github.com>
2024-02-04 06:22:12 +0000

    Variable and define

---
+++ hello.c
@@ -0,0 +1,35 @@
+/*
+We will learn in next chapter:
+1. Variable
+2. Keywords
+3. Constants
+4. Comments 
+5. Structure
+6. Compilation
+*/
+/*
+
+1. Variable :- 
+Vairable is the name of a memory location which stores some data.
+a. Variable are Case sensitive
+b. 1st character is alphabet or '-'
+c. No comma/ blank space
+d. No other symbol other than '_'
+
+
+*/
+#include <stdio.h>
+int main(){
+    int number = 25;
+    char star = '*';
+    float pi = 3.14;
+
+    int a = 30;
+    int A = 40;
+
+    int _age = 22;
+
+    int final_price = 100;
+    
+    return 0;
+}
